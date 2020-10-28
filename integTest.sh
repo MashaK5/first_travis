3d81cc07f24b6822037ef61a8784041caf14cde2
Maria Kozlovtseva <mashak1407@gmail.com.com>
2020-10-28 23:18:01 +0300

    Travis3

--- integTest.sh
+++ integTest.sh
@@ -4,8 +4,8 @@ gradle jar
 
 for (( test = 0; test < 10; test++ ))
 do
-java -jar build/libs/project_2.jar "data/example${test}.txt" output.txt
-diff -q "data/results/example${test}.txt" output.txt
+java -jar build/libs/project_2.jar "data/example${test}.txt" "data/example${test}.o"
+diff -q "data/results/example${test}.txt" "data/example${test}.o"
 if [ $? -eq 1 ]
 then
 echo "test $test failed"
